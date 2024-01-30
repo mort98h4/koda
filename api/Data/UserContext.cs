dd4a27f1281d520c5c6283d84af5dcf8ca05ab6b
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2024-01-30 19:55:15 +0100

    Created UserContext

---
+++ api/Data/UserContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<api.Models.User> User { get; set; } = null!;
+    }
+}
