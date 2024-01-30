b696c583a1cace72f5da599def5eba11fb83882a
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2024-01-30 18:17:13 +0100

    Created User model

---
+++ api/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string? FistName { get; set; }
+    public string? LastName { get; set; }
+    public string? ArtistName { get; set; }
+    public string? Email { get; set; }
+}
