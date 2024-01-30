203bc624a677dd2ed87a97b855f481855680e045
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2024-01-30 21:54:31 +0100

    Added validation to User model

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -1,10 +1,28 @@
-﻿namespace api.Models;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace api.Models;
 
 public class User
 {
     public long Id { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
+    [Display(Name = "First name")]
     public string? FirstName { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
+    [Display(Name = "Last name")]
     public string? LastName { get; set; }
+
+    [StringLength(50, MinimumLength = 2)]
+    [Display(Name = "Artist name")]
     public string? ArtistName { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string? Email { get; set; }
 }
