b3c31122ce19a117aeb1b352043b517c78e97b14
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2024-02-02 15:21:56 +0100

    Created Main component

---
+++ ui/src/app/components/Main.js
@@ -0,0 +1,7 @@
+export function MainEl({children}) {
+    return (
+        <main className='flex min-h-screen flex-col items-center p-24 relative'>
+            {children}
+        </main>
+    )
+}
