db5630d674a8253e7f41c168f82f87d60141393f
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2024-02-02 12:55:47 +0100

    Added FromForm attributes

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Models;
 
@@ -11,18 +12,22 @@ public class User
     [Required]
     [StringLength(50, MinimumLength = 2)]
     [Display(Name = "First name")]
+    [FromForm(Name = "firstName")]
     public string FirstName { get; set; } = "";
 
     [Required]
     [StringLength(50, MinimumLength = 2)]
     [Display(Name = "Last name")]
+    [FromForm(Name = "lastName")]
     public string LastName { get; set; } = "";
 
     [StringLength(50)]
     [Display(Name = "Artist name")]
+    [FromForm(Name = "artistName")]
     public string? ArtistName { get; set; } = "";
 
     [Required]
     [EmailAddress]
+    [FromForm(Name = "email")]
     public string Email { get; set; } = "";
 }
